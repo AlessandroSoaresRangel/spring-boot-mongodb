1276e973a5c28a1db2f2f86d2e177ce2bee402c9
Alessandro <alessandrosoaresrangel1@gmail.com>
2024-01-06 15:12:59 -0300

    Creating entity Post

---
+++ src/main/java/com/project/mongo/repositories/PostRepository.java
@@ -0,0 +1,11 @@
+package com.project.mongo.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.mongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
